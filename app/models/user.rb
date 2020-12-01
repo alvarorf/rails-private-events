b956a6a33c3b5775e87373d2eb56134386f6ea86
alvaro <aaruizf@unal.edu.co>
2020-12-01 12:56:59 -0500

    Add simple_form gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  validades :name, presence: true, uniqueness: false, length: { in: 4...100 }
+  validates :name, presence: true, uniqueness: false, length: { in: 4...100 }
   validates :username, presence: true, uniqueness: true, length: { in: 5...254 }
   validates :email, presence: true, uniqueness: true, length: { in: 10..254 }, format:
   {
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { in: 8..30 }
   belongs_to :user
   has_many :attendances
-  has_many :attendees, through: attendances
+  has_many :events, through: :attendances
 end
