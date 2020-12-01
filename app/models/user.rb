14df097d1f8d245c4ef46e80326d1605c6c17924
alvaro <aaruizf@unal.edu.co>
2020-12-01 12:14:17 -0500

    Create event, attendance, and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   validades :name, presence: true, uniqueness: false, length: { in: 4...100 }
   validates :username, presence: true, uniqueness: true, length: { in: 5...254 }
   validates :email, presence: true, uniqueness: true, length: { in: 10..254 }, format:
@@ -6,5 +7,7 @@ class User < ApplicationRecord
     with: URI::MailTo::EMAIL_REGEXP
   }
   validates :password, presence: true, length: { in: 8..30 }
-  has_and_belongs_to_many :events
+  belongs_to :user
+  has_many :attendances
+  has_many :attendees, through: attendances
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_220930) do
+ActiveRecord::Schema.define(version: 2020_12_01_165233) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
