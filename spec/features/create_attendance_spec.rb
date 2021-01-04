e16ce510efb6dc395f0ef4bab745bcbeb4b06f08
alvaro <aaruizf@unal.edu.co>
2021-01-04 13:00:14 -0500

    Add some negative tests to the log in feature

--- spec/features/create_attendance_spec.rb
+++ spec/features/create_attendance_spec.rb
@@ -21,4 +21,25 @@ RSpec.describe 'Create attendance to event from logged in user', type: :feature 
     find('#attendance_event_id').find(:xpath, 'option[1]').select_option
     click_button 'Create Attendance'
   end
+
+  scenario 'Create attendance to an existing event' do
+    User.create(name: 'example', username: 'example', email: 'example@example.com', password: 'password1234')
+    visit login_url
+    fill_in 'email', with: 'example@example.com'
+    fill_in 'password', with: 'password1234'
+    click_button 'Log in'
+    visit '/'
+    expect(page).to have_content 'example'
+    visit new_event_url
+    fill_in 'event[title]', with: 'sample event'
+    fill_in 'event[date]', with: '2021-02-30'
+    fill_in 'event[location]', with: 'somewhere in the multiverse'
+    fill_in 'event[description]', with: 'This is a sample event'
+    click_button 'Create Event'
+    Event.create(title: 'sample event', date: '2021-02-30',
+                 location: 'somewhere in the multiverse', description: 'This is a sample event')
+    visit new_attendance_url
+    find('#attendance_event_id').find(:xpath, 'option[1]').select_option
+    click_button 'Create Attendance'
+  end
 end
--- spec/features/login_user_spec.rb
+++ spec/features/login_user_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe 'Authenticate user', type: :feature do
     visit '/'
     expect(page).to have_content 'example'
   end
+
+  scenario 'It should fail to authenticate the user if the user does not exist' do
+    User.create(name: 'example', username: 'example', email: 'example@example.com', password: 'password1234')
+    visit login_url
+    fill_in 'email', with: 'example2@example.com'
+    fill_in 'password', with: 'password1234'
+    click_button 'Log in'
+    expect(page).to have_content 'incorrect'
+  end
+
+  scenario 'It should not show another username different from the current user' do
+    User.create(name: 'example', username: 'example', email: 'example@example.com', password: 'password1234')
+    visit login_url
+    fill_in 'email', with: 'example@example.com'
+    fill_in 'password', with: 'password1234'
+    click_button 'Log in'
+    expect(page).to_not have_content 'example2'
+  end
 end
